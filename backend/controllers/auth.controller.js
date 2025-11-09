2212da83f11b910666a093131d5fb51267b212cb
INBAVARUNAN-S <inbavarunans.bcy24@rathinam.in>
2025-11-10 01:06:41 +0530

    login,signUp,logOut finished

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -8,7 +9,7 @@ export const signup = async (req, res) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       return res.status(400).json({ error: "Invalid Email Format" });
-    }  
+    }
 
     const existingEmail = await User.findOne({ email });
     const existingUserName = await User.findOne({ userName });
@@ -24,51 +25,87 @@ export const signup = async (req, res) => {
 
     // hashing the password
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt); 
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
-        userName,
-        fullName,
-        email,
-        password:hashedPassword
+      userName,
+      fullName,
+      email,
+      password: hashedPassword,
+    });
 
-    })
-
-    if(newUser){
-      generateToken(newUser._id, res) 
-        await newUser.save();
-        res.status(200).json({
-            _id: newUser._id,
-            userName:newUser.userName,
-            fullName:newUser.fullName,
-            email:newUser.email,
-            followers:newUser.followers,
-            following:newUser.following,
-            profileImg:newUser.profileImg,
-            coverImg:newUser.coverImg,
-            bio:newUser.bio,
-            link:newUser.link
-        }) 
-    }else{
-        res.status(400).json({error:"Invalid user data"})
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(200).json({
+        _id: newUser._id,
+        userName: newUser.userName,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+        bio: newUser.bio,
+        link: newUser.link,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
     }
-
   } catch (error) {
     console.log(`Error in signup controller : ${error}`);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
   try {
+    const { userName, password } = req.body;
+    const user = await User.findOne({ userName });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      userName: user.userName,
+      fullName: user.fullName,
+      email: user.email,
+      followers: user.followers,  
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+      bio: user.bio,
+      link: user.link,
+    });
   } catch (error) {
     console.log(`Error in login controller : ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-export const logout = (req, res) => {
+export const logout = async (req, res) => {
   try {
+    res.cookie("jwt","",{maxAge:0});
+    res.status(200).json({message:"Logout Successfully"})
   } catch (error) {
-    console.log(`Error in logout controller : ${error}`);
+    console.log(`Error in Logout Controller ${error}`);
+    res.status(500).json({error:"Internal Server Error"})
   }
 };
+
+export const getMe = async () => {
+  try{
+    const user = await User.findOne({_id:req.user._id})
+  }catch(error){
+    console.log(`Error in getMe controller ${error}`)
+    res.status(500).json({error:"Internal Server Error"})
+  }
+}
