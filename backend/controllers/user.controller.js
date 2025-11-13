d37783d15d9c22cbfaf0a4e52feafeccac877c19
INBAVARUNAN-S <inbavarunans.bcy24@rathinam.in>
2025-11-13 20:53:34 +0530

    updated

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,7 @@
 import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 
 export const getProfile = async (req, res) => {
   try {
@@ -70,9 +72,45 @@ export const getSuggestedUsers = async (req,res) => {
           size : 10
         }
       }
-    ])
+    ]) 
+    const filteredUser = users.filter((user) => !userFollowedByMe.following.includes(user._id))
+    const suggestedUsers = filteredUser.slice(0,4);
+
+    suggestedUsers.forEach((user) => (user.password = null)) 
+    res.status(200).json(suggestedUsers);
+
   }catch(error){
     console.log(`Error in Get SuggestedUsers Controller : ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export const updateUser = async (req,res) =>{
+  try {
+    const userId = req.user._id;
+    const {userName, fullName ,email ,currentPassword ,newPassword ,bio , link} = req.body;
+    const user = await User.findById({_id:userId})
+    if(!user){
+      return res.status(400).json({error:"User not found"})
+    }
+
+    if((!newPassword && currentPassword) || (!currentPassword && newPassword)){
+      return res.status(400).json({error:"Please provide both newpassword and currentpassword"})
+    }
+
+    if(currentPassword && newPassword){
+      const isMatch = await bcrypt.compare(currentPassword, user.password)
+      if(!isMatch){
+        return res.status(400).json({error:"Current Password is Incorrect"})
+      }
+      if(newPassword.length<6){
+        return res.status(400).json({error:"Password must have atleast 6 characters"})
+      }
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword,salt);
+    }
+  } catch (error) {
+    console.log(`Error in Get Updated User Controller : ${error}`);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
