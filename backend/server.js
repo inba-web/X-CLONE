56b6a8369577a92fa5494f25041d5b957e93de21
Inba <inba.web@gmail.com>
2025-12-18 23:11:30 +0530

    fixed minor bugs

--- backend/server.js
+++ backend/server.js
@@ -29,12 +29,12 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
